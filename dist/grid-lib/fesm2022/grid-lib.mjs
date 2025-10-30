a1bbdc1a06a0b5ac310800fc45f1e49d5d7345f7
rajvinavlakha-inventyv <rajvi.navlakha@inventyv.com>
2025-10-30 18:23:36 +0530

    Changes

--- dist/grid-lib/fesm2022/grid-lib.mjs
+++ dist/grid-lib/fesm2022/grid-lib.mjs
@@ -8,11 +8,11 @@ class Grid {
         console.log("My Comp works!");
     }
     static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.7", ngImport: i0, type: Grid, deps: [], target: i0.ɵɵFactoryTarget.Component });
-    static ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "20.3.7", type: Grid, isStandalone: true, selector: "lib-grid", ngImport: i0, template: "<p>grid works!</p>\n", styles: [""], dependencies: [{ kind: "ngmodule", type: CommonModule }] });
+    static ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "20.3.7", type: Grid, isStandalone: true, selector: "lib-grid", ngImport: i0, template: "<p>grid works!</p>\nHi", styles: [""], dependencies: [{ kind: "ngmodule", type: CommonModule }] });
 }
 i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.7", ngImport: i0, type: Grid, decorators: [{
             type: Component,
-            args: [{ selector: 'lib-grid', imports: [CommonModule], standalone: true, template: "<p>grid works!</p>\n" }]
+            args: [{ selector: 'lib-grid', imports: [CommonModule], standalone: true, template: "<p>grid works!</p>\nHi" }]
         }], ctorParameters: () => [] });
 
 /*
